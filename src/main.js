9f1d916cb05c38fc7750a5feae1b91bf5e931246
Davi Moura <davisilvag4mer@gmail.com>
2023-08-25 23:10:19 -0300

    feat: configure pinia

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import 'ant-design-vue/dist/reset.css'
+
+import { createPinia } from 'pinia'
 import { createRouter, createWebHashHistory } from 'vue-router/auto'
 
+const pinia = createPinia()
 const router = createRouter({
   history: createWebHashHistory()
 })
 
 const app = createApp(App)
+app.use(pinia)
 app.use(router)
 app.mount('#app')
