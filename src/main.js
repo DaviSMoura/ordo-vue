c6e59df0c5b1916e3eaec04fdbbb533e33bdc768
Davi Moura <davisilvag4mer@gmail.com>
2023-08-25 22:59:18 -0300

    chore: clean project & install ant

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import 'ant-design-vue/dist/reset.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
