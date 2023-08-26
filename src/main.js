f9bdc46b58e3f2d3c9dc601e2944a2d9a8fcdb70
Davi Moura <davisilvag4mer@gmail.com>
2023-08-25 23:08:31 -0300

    feat: config auto routes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import 'ant-design-vue/dist/reset.css'
+import { createRouter, createWebHashHistory } from 'vue-router/auto'
+
+const router = createRouter({
+  history: createWebHashHistory()
+})
 
 const app = createApp(App)
+app.use(router)
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter({}),
     vue(),
     Components({
       resolvers: [
