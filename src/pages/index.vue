6374a85e5f5b51f0f423c1c5954eed41f21c475e
Davi Moura <davisilvag4mer@gmail.com>
2023-08-28 23:59:34 -0300

    feat: basic landing page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,13 @@
+<script setup>
+import { SmileTwoTone } from '@ant-design/icons-vue'
+</script>
+
 <template lang="pug">
-a-button(type='primary') Botão de testes
+a-row(align="center")
+  a-col
+    a-result(title="Bem vindo a Plataforma Ordo!" sub-title="Esta plataforma foi construida sob medida para resolver demandas pessoais do Davi Moura.")
+      template(#icon)
+        smile-two-tone
 </template>
 
 <route lang="yaml">
